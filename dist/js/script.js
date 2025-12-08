94e5831b68d31566a68ba98aa5ff65dff9f9426c
deSlave <100181026+deSlave@users.noreply.github.com>
2025-12-08 13:33:09 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,54 @@
+const menuBar = document.querySelector(".hamburger");
+const menuNav = document.querySelector(".navbar");
+const menuNavigation = document.querySelector(".menu");
+const buttons = document.querySelectorAll(".details-btn");
+const closes = document.querySelectorAll(".popup .close");
+
+
+menuBar.addEventListener("click", () => {
+  menuBar.classList.toggle("is-active");
+  menuNavigation.classList.toggle("menu-active");
+});
+
+window.addEventListener("scroll", () => {
+  menuNav.classList.toggle("nav-scroll-active", window.scrollY > 0);
+
+  windowsPosition = window.scrollY > 200;
+  menuBar.classList.toggle("btn-active", windowsPosition);
+  menuBar.classList.remove("is-active");
+  menuNavigation.classList.remove("menu-active");
+});
+
+const loader = document.querySelector(".loader");
+
+function loaderActive() {
+  loader.classList.add("loader-active");
+}
+
+function loaderActiveTime() {
+  setInterval(loaderActive, 3000);
+}
+
+window.onload = loaderActiveTime();
+
+// Buka popup
+buttons.forEach(btn => {
+  btn.addEventListener("click", function() {
+    const target = this.getAttribute("data-target");
+    document.getElementById(target).style.display = "flex";
+  });
+});
+
+// Tutup popup
+closes.forEach(close => {
+  close.addEventListener("click", function() {
+    this.parentElement.parentElement.style.display = "none";
+  });
+});
+
+// Klik luar popup = close
+window.addEventListener("click", function(e) {
+  if (e.target.classList.contains("popup")) {
+    e.target.style.display = "none";
+  }
+});
